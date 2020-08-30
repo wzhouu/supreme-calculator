b7363e55208b16165e5fa73d34cae6c9d654947b
Wesley Zhou <68358562+TheNoobWeasel@users.noreply.github.com>
2020-08-29 23:23:18 -0700

    Update calculator.rb

--- calculator.rb
+++ calculator.rb
@@ -1,33 +1,27 @@
-puts "Welcome to Premier Zhou's Calculator v1.0.0"
+puts "Welcome to Premier Zhou's Calculator v1.0.1"
 
-#1st Number user enters
-puts "Enter a number: "
-num1 = gets.chomp().to_f
+while true
+  #1st Number user enters
+  puts "Enter a number: "
+  num1 = gets.chomp().to_f
 
-#2nd number user enters
-puts "Enter another number: "
-num2 = gets.chomp().to_f
+  #2nd number user enters
+  puts "Enter another number: "
+  num2 = gets.chomp().to_f
 
-#sign user enters
-puts "Enter sign: "
-sign = gets.chomp()
+  #sign user enters
+  puts "Enter sign: "
+  sign = gets.chomp()
 
-#addition
-if sign == "+"
-  puts (num1 + num2)
-end
-
-#subtraction
-if sign == "-"
-  puts (num1 - num2)
-end
-
-#multiplication
-if sign == "*"
-  puts (num1 * num2)
-end
-
-#division
-if sign == "/"
-  puts (num1 / num2)
+  if sign == "+"
+    puts (num1 + num2)
+  elsif sign == "-"
+    puts (num1 - num2)
+  elsif sign == "*"
+    puts (num1 * num2)
+  elsif sign == "/"
+    puts (num1 / num2)
+  else
+    puts "That is not a valid sign!"
+  end
 end

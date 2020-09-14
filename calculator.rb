32286b87b06c326fb11fbc5165ba1a1f49e52493
Wesley Zhou <68358562+TheNoobWeasel@users.noreply.github.com>
2020-09-13 17:13:36 -0700

    v1.3.0
    -Compiled calculator.exe
    -Updated and added methods
    -Updated continuation if statement

--- calculator.rb
+++ calculator.rb
@@ -1,47 +1,63 @@
-puts "Welcome to Premier Zhou's Supreme Calculator v1.2.0"
+def add(num1, num2)
+  num1 + num2
+end
+
+def subtract(num1, num2)
+  num1 - num2
+end
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+def divide(num1, num2)
+  num1 / num2
+end
+
+def exponent(num1, num2)
+  num1 ** num2
+end
+
+puts "Welcome to Premier Zhou's Supreme Calculator v1.3.0"
 
 #welcome message
 puts "Enter your name: "
-name = gets.chomp()
+name = gets.chomp
 puts "Hello #{name}! You are using a Supreme Invention from the Premier."
 
 #calculator power
 while true
   #1st Number user enters
   puts "Enter a number: "
-  num1 = gets.chomp().to_f
+  num1 = gets.chomp.to_f
 
   #2nd number user enters
   puts "Enter another number: "
-  num2 = gets.chomp().to_f
+  num2 = gets.chomp.to_f
 
   #sign user enters
   puts "Enter sign: "
-  sign = gets.chomp()
+  sign = gets.chomp
 
   #signs-and-answers
   if sign == "+"
-    puts (num1 + num2)
+    puts add(num1, num2)
   elsif sign == "-"
-    puts (num1 - num2)
+    puts subtract(num1, num2)
   elsif sign == "*"
-    puts (num1 * num2)
+    puts multiply(num1, num2)
   elsif sign == "/"
-    puts (num1 / num2)
+    puts divide(num1, num2)
   elsif sign == "**"
-    puts (num1 ** num2)
+    puts exponent(num1, num2)
   else
     puts "That is not a valid sign!"
   end
 
-puts "Would you like to keep calculating? Yes/No "
-continue = gets.chomp()
-  if continue == "Yes"
-    next
-  elsif continue == "No"
-    puts "Thank you for using the Premier's Supreme Calculator. Have a great day!"
-    break
-  else
+  #continue-calculating
+  puts "Would you like to keep calculating? Yes/No "
+  continue = gets.chomp.upcase
+  if continue == "NO"
     puts "Thank you for using the Premier's Supreme Calculator. Have a great day!"
     break
   end
